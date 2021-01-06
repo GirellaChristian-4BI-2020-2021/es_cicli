54ff052397caa7397fe5f329f168e89c20a85a76
GirellaChristian-4BI-2020-2021 <73601255+GirellaChristian-4BI-2020-2021@users.noreply.github.com>
2021-01-06 15:55:51 +0100

    Add files via upload

---
+++ ControlloInput_01.java
@@ -0,0 +1,22 @@
+
+package controlloinput_01;
+
+import java.util.Scanner;
+
+public class ControlloInput_01 {
+
+    
+    public static void main(String[] args) {
+        int numero = 0, somma = 0;
+        Scanner input = new Scanner(System.in);
+        do{
+            System.out.println("Inserisci il nuovo numero");
+            numero = input.nextInt();
+            if(numero>0){
+            somma += numero;
+            }
+        }while(numero!=0);
+        System.out.println("La somma è " + somma);
+    }
+    
+}
